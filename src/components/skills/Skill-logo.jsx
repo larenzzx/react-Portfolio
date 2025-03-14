6a5cd33a7cd10bf02ef79dca744363cc8e88710a
larenzz <larenztabotabo@gmail.com>
2025-03-14 19:22:27 +0800

    logo size updated

--- src/components/skills/Skill-logo.jsx
+++ src/components/skills/Skill-logo.jsx
@@ -2,7 +2,7 @@ export const SkillLogo = ({ logos, tooltip }) => {
   return (
     // <img src={logos} className="max-w-14 md:max-w-20" alt="" />
     <div
-      className="tooltip tooltip-primary flex size-20 items-center justify-center rounded-2xl border border-[rgba(255,255,255,0.2)] bg-gray-400 bg-opacity-10 bg-clip-padding p-3 backdrop-blur-md backdrop-contrast-100 backdrop-saturate-100 backdrop-filter transition duration-300 ease-in hover:scale-105 hover:border-[rgba(255,255,255,0.5)] hover:bg-opacity-20 hover:shadow-lg md:size-24"
+      className="tooltip tooltip-primary flex size-20 items-center justify-center rounded-2xl border border-[rgba(255,255,255,0.2)] bg-gray-400 bg-opacity-10 bg-clip-padding p-3 backdrop-blur-md backdrop-contrast-100 backdrop-saturate-100 backdrop-filter transition duration-300 ease-in hover:scale-105 hover:border-[rgba(255,255,255,0.5)] hover:bg-opacity-20 hover:shadow-lg md:size-28"
       data-tip={tooltip}
     >
       <img src={logos} className="size-20 md:size-24" alt={tooltip} />
